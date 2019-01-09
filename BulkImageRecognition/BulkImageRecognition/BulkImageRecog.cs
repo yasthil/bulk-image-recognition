283155f69564a140b369764baa733a4a29e07059
Yasthil Bhagwandeen <deiago147@gmail.com>
2019-01-08 17:33:22 -0800

    Added sample code and basic readme

---
+++ BulkImageRecognition/BulkImageRecognition/BulkImageRecog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.Vision.V1;
+
+namespace BulkImageRecognition
+{
+    class BulkImageRecog
+    {
+        public void Run()
+        {
+            // Load an image from a local file.
+            var filePath = "/PATH/TO/IMAGE.png";
+            var image = Image.FromFile(filePath);
+            var client = ImageAnnotatorClient.Create();
+            var response = client.DetectLabels(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
+        }
+      
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Quick Start Guide:
+https://cloud.google.com/vision/docs/quickstart-client-libraries
+
+Setup the C# library using this command:
+``` Install-Package Google.Cloud.Vision.V1 -Pre ```
+
+NB: Setup the Create service account key
+
+Follow instructions here:
+https://cloud.google.com/vision/docs/detecting-labels
+
+Setup Google Cloud Project and authentication:
+https://cloud.google.com/vision/docs/labels
+
+NB: Remember to install and initialize the Cloud SDK
+You'll also need to link to your Google Cloud Project during this process.
+
